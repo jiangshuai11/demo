376390aa7a2e9158548c8eee34a336270a15552f
jiangshuai <jiangshuai@broada.com>
2020-02-26 23:04:14 +0800

    测试vvvv

--- src/main/java/com/example/demo/wwwww/cccc.java
+++ src/main/java/com/example/demo/wwwww/cccc.java
@@ -7,4 +7,8 @@ package com.example.demo.wwwww;
  * @create: 2020-02-26 22:34
  */
 public class cccc {
+
+    public static void main(String[] args) {
+        System.out.println("111111111111111");
+    }
 }
